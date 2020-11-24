9a82a6e5ebd31894428ac986300e7a82f27579ad
Daniel Ariza Suarez <daarizasu@unal.edu.co>
2020-11-24 12:42:11 -0500

    Create create_MrBean_shorcut.R

---
+++ docs/create_MrBean_shorcut.R
@@ -0,0 +1,21 @@
+cdir <- choose.dir()
+
+dir.create(paste0(cdir, '/MrBean_exe'))
+
+cdir <- paste0(cdir, '/MrBean_exe')
+
+setwd(cdir)
+
+writeLines( c("@echo off",
+              paste0('start /min "Running MrBean" "',
+                     paste0(R.home(), '/bin/Rscript'),
+                     '" -e "library(MrBean) ; run_app() ; quit(save = FALSE)" ')),
+            con = paste0( file.path(cdir, '/MrBean.bat' ) ) )
+
+download.file(url = 'https://raw.githubusercontent.com/AparicioJohan/Starting_MrBean/main/images/icon.ico',
+              destfile = paste0(cdir, '/icon.ico'), mode = 'wb')
+
+download.file(url = 'https://raw.githubusercontent.com/AparicioJohan/Starting_MrBean/main/docs/link.vbs',
+              destfile = paste0(cdir, '/link.vbs'), mode = 'wb')
+
+shell( '.\\link.vbs .\\MrBean.lnk .\\MrBean.bat .\\icon.ico' )
